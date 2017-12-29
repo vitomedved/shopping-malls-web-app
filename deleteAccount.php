e2c1632240b2b3d01a4422ee24bf5099caa0ff9a
vitomedved <vito.medved12@gmail.com>
2017-12-29 17:58:24 +0100

    Add register, delete and userFunctions
    Add abilities to register, delete account.
    Add userFunctions.php with only 1 function to get userId. Will probably delete file later and add function to login.php

---
+++ deleteAccount.php
@@ -0,0 +1,31 @@
+<?php
+
+include 'connectionToDB.php';
+
+session_start();
+
+$link = connectToDB();
+
+if(!$link)
+{
+	echo("Ne mogu se spojiti s bazom podataka.");
+}
+else
+{
+	$query = "DELETE FROM `korisnik` WHERE `korisnik`.`id_korisnik` = ".$_SESSION['userId'].";";
+	$result = mysqli_query($link, $query);
+	if(!$result)
+	{
+		echo("Error with query");
+	}
+	else
+	{
+		echo("Account deleted, gg wp");
+		session_unset();
+		session_destroy();
+	}
+}
+
+header("Location: /RWA_ducani/index.php");
+
+?>
